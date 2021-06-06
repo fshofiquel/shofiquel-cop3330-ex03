390a1d1212e46cc6178854959c2512c50b1faa72
fshofiquel <fshofique@gmail.com>
2021-06-05 20:06:03 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Fazlur Shofiquel
+ */
+
+package base;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        Scanner nameInput = new Scanner(System.in);
+        Scanner quoteInput = new Scanner(System.in);
+        String quote;
+        String name;
+        String outputString;
+
+        System.out.print("What is the quote? ");
+        quote = quoteInput.nextLine();
+        System.out.print("Who said it? ");
+        name = nameInput.nextLine();
+
+        outputString = "" +name+ " says, " +quote;
+    }
+
+}
